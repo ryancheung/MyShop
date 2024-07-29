842853f4ed8385579ea94ab316910934593ecb30
ryancheung <ryancheung.go@gmail.com>
2024-07-29 11:40:04 +0800

    Aspire

---
+++ src/eShop.AppHost/Program.cs
@@ -0,0 +1,8 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var postgres = builder.AddPostgres("postgres").WithPgAdmin();
+var catalogDb = postgres.AddDatabase("CatalogDB");
+
+builder.AddProject<Projects.Catalog_Data_Manager>("catalog-db-mgr").WithReference(catalogDb);
+
+builder.Build().Run();

cd14c3aa3de5d1a9304c59256358def7bbbf263b
ryancheung <ryancheung.go@gmail.com>
2024-07-30 07:42:27 +0800

    Web app

---
+++ src/WebApp/Services/LogOutService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace eShop.WebApp.Services;
+
+public class LogOutService
+{
+    public async Task LogOutAsync(HttpContext httpContext)
+    {
+        await Task.CompletedTask;
+    }
+}
--- src/eShop.AppHost/Program.cs
+++ src/eShop.AppHost/Program.cs
@@ -4,6 +4,7 @@ var postgres = builder.AddPostgres("postgres").WithPgAdmin();
 var catalogDb = postgres.AddDatabase("CatalogDB");
 
 builder.AddProject<Projects.Catalog_Data_Manager>("catalog-db-mgr").WithReference(catalogDb);
-builder.AddProject<Projects.Catalog_API>("catalog-api").WithReference(catalogDb);
+var catalogApi = builder.AddProject<Projects.Catalog_API>("catalog-api").WithReference(catalogDb);
+builder.AddProject<Projects.WebApp>("webapp").WithReference(catalogApi);
 
 builder.Build().Run();
